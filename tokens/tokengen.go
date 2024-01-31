9bc256ed1527e95c8f92f4c0569331030198d93d
ammar <ammarnousher56@gmail.com>
2024-02-01 00:28:39 +0500

    [done] validate token and update all tokens function

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,13 +1,15 @@
 package tokens
 
 import (
+	"log"
 	"os"
 	"time"
-	"log"	
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"github.com/ammar-nousher-ali/ecommerce/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
@@ -86,12 +88,87 @@ func TokenGenerator(email string, firstname string, lastname string, uid string(
 
 
 
-func ValidateToken(){
+func ValidateToken(signedtoken string)(claims *SignedDetails, msg string){
 
+	token, err := jwt.ParseWithClaims(signedtoken, &SignedDetails{}, func(token *jwt.Token)(interface{}, error ){
+
+		return []byte(SECRET_KEY), nil
+
+	})
+
+
+
+	if err!=nil {
+
+		msg = err.Error()
+		return
+	}
+
+
+	claims, ok := token.Claims(*SignedDetails)
+	if !ok {
+
+		msg = "the token in invalid"
+		return
+		
+	}
+
+
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+
+		msg = "Token is already expired"
+		return
+	}
+
+	return claims, msg
 }
 
 
-func UpdateAllTokens(){
+func UpdateAllTokens(signedtoken string, signedrefreshedtoken string, userid string){
+
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var updateobj primitive.D
+
+
+	updateobj = append(updateobj, bson.E{Key : "token", Value: signedtoken})
+	updateobj = append(updateobj, bson.E{Key : "refresh_token", Value: signedrefreshedtoken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	updateobj = append(updateobj, bson.E{Key : "updatedat", Value: updated_at})
+
+	upsert := true
+
+	filter := bson.M{"user_id": userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+
+	}
+
+	_, err := UserData.UpdateOne(ctx, filter, bson.D{
+		{Key:"$set", Value:updateobj},
+	},
+	&opt)
+
+
+	defer cancel()
+
+
+	if err!=nil {
+
+		log.Panic(err)
+		return
+		
+	}
+
+
+
+
+
+
+
+
 
 
 }
