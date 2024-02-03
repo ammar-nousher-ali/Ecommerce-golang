7b770b4c348c69f0bb3d1468d2bca5af38b84641
ammar <ammarnousher56@gmail.com>
2024-02-03 19:37:05 +0500

    [done] remove item from cart function

--- database/cart.go
+++ database/cart.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	ErrCantFindProduct   = errors.New("can't find the product")
+	ErrCantFindProduct    = errors.New("can't find the product")
 	ErrCantDecodeProducts = errors.New("can't find the product")
 	ErrUserIdIsNotValid   = errors.New("this user is not valid")
 	ErroCantUpdateUser    = errors.New("cannot add this product to the cart")
@@ -24,50 +24,63 @@ var (
 func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
 
 	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id": productID})
-	if err!=nil {
+	if err != nil {
 		log.Println(err)
 		return ErrCantFindProduct
-		
+
 	}
 
 	var productCart []models.ProductUser
 	err = searchfromdb.All(ctx, &productCart)
-	if err!=nil {
+	if err != nil {
 		log.Println(err)
 		return ErrCantDecodeProducts
-		
+
 	}
 
-
 	id, err := primitive.ObjectIDFromHex(userID)
-	if err!=nil {
+	if err != nil {
 
 		log.Println(err)
 		return ErrUserIdIsNotValid
-		
-	}
 
+	}
 
 	filter := bson.D{primitive.E{Key: "_id", Value: id}}
 	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
 
-
 	_, err = userCollection.UpdateOne(ctx, filter, update)
-	if err!=nil {
+	if err != nil {
 
 		return ErroCantUpdateUser
-		
+
 	}
 
 	return nil
 
-
-
-
 }
 
-func RemoveCartItem() {
+func RemoveCartItem(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
 
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+
+		log.Println(err)
+		return ErrUserIdIsNotValid
+
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.M{"$pull":bson.M{"usercart": bson.M{"_id":productID}}}
+
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+
+	if err!=nil {
+		return ErrCantRemoveItemCart
+		
+	}
+
+	return nil
 }
 
 func BuyItemFromCart() {
